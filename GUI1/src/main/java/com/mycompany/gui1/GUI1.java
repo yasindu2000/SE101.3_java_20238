1a03d557950646b37c9236f932d22f1eafb4b461
yasindu dahanayaka <135942789+yasindu2000@users.noreply.github.com>
2023-07-20 18:43:54 +0530

    Lab 11-GUI Q2-2023.07.20

---
+++ GUI1/src/main/java/com/mycompany/gui1/GUI1.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.gui1;
+
+public class GUI1 {
+
+    public static void main(String[] args) 
+    {
+        ClickCounter cc=new ClickCounter();
+        cc.show();
+    }
+}
