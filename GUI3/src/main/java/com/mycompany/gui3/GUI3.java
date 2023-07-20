ba30015b96fe1afe99e25e46ccf42d901cd11186
yasindu dahanayaka <135942789+yasindu2000@users.noreply.github.com>
2023-07-20 22:31:02 +0530

    Lab 11-GUI Q3-2023.07.20

---
+++ GUI3/src/main/java/com/mycompany/gui3/GUI3.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.gui3;
+
+public class GUI3 {
+
+    public static void main(String[] args)
+    {
+        ContactEditor ce=new ContactEditor();
+        ce.show();
+    }
+}
